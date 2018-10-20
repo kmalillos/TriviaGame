e02934034bf67ef57f23d7d255060399ea7d91f7
kmalillos <malillosk@gmail.com>
2018-10-20 15:40:57 -0600

    added displayTrivia function in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,36 +4,40 @@ $(document).ready(function () {
 
 // --- Global Variables ---
 
-
 var timer = 30; 
 var interval;
-var userScore = 0;
-var triviaGame = [
-    {   question: "question" , 
-        choices: ["choice1", "choice2", "choice3", "choice4"] ,
-        answer: "answer" // index number
+
+var triviaGame = [ 
+    {   question: "What is Steve Rogers' superhero name?" , 
+        choices: ["Spiderman", "Dr. Strange", "Captain America", "Winter Soldier"] ,
+        answer: "Captain America" // 3
     } , 
-    {   question: "question" , 
-        choices: ["choice1", "choice2", "choice3", "choice4"] ,
-        answer: "answer" // index number
+    {   question: "What color is the Hulk?" , 
+        choices: ["purple", "green", "red", "blue"] ,
+        answer: "green" // 2
     } , 
-    {   question: "question" , 
-        choices: ["choice1", "choice2", "choice3", "choice4"] ,
-        answer: "answer" // index number
+    {   question: "Who is the God of Thunder?" , 
+        choices: ["Loki", "Thor", "Ironman", "Hulk"] ,
+        answer: "Thor" // 2
     } , 
-    {   question: "question" , 
-        choices: ["choice1", "choice2", "choice3", "choice4"] ,
-        answer: "answer" // index number
+    {   question: "Where is the Black Panther from?" , 
+        choices: ["Ultron", "Sokovia", "Asgaard", "Wakanda"] ,
+        answer: "Wakanda" // 4
     } 
 ]
 
+var correct = 0;
+var incorrect = 0;
+
+
 // --- Functions ---
 
-    // Timer Functions
+    // to start timer
 function startTimer () {
         interval = setInterval(decrement, 1000); // decrease by 1 sec
 }
 
+    // to countdown timer
 function decrement () {
     timer--;
     $("#timer-text").text(timer);
@@ -47,8 +51,41 @@ function decrement () {
     }
 }
 
+    // to stop timer
 function stop () {
     clearInterval(interval);
+}
+
+function startGame() {
+    $(".correct-holder").empty();
+    $(".incorrect-holder").empty();
+    correct = 0;
+    incorrect = 0;
+}
+
+function displayTrivia () {
+
+    var triviaText = $(".trivia-text");
+
+    for (var i=0; i < triviaGame.length; i ++) {
+        
+        var questionHolder = $("<div>");
+            questionHolder.addClass("question");
+            questionHolder.attr('id', ("question-"+i));
+            questionHolder.append(triviaGame[i].question)     
+        
+        triviaText.append(questionHolder);
+
+        for (var j=0; j < triviaGame.length; j++) {
+            var choicesHolder = $("<input>");
+                choicesHolder.addClass("choice");
+                choicesHolder.attr('id', ("question-"+i+"-choice-"+j));
+                choicesHolder.append(triviaGame[i].choices[j]);
+        triviaText.append(choicesHolder);
+        }
+        
+    }   
+
 }
 
 
@@ -67,15 +104,18 @@ $("#start-button").on("click", function() {
 
 startTimer();
 
-$("#trivia-text").text("Trivia Game Here!");
+startGame();
+
+displayTrivia();
 
 
-    // trivia ends when submitted (- or when time runs out, another function)
+// trivia ends when submitted (- or when time runs out, another function)
 $("#submit-button").on("click", function() {
     $(this).hide();   // submit button hides
     $(".trivia-page").hide(); // trivia page hides
     $(".results-page").show(); // results page shows
     $("#results-text").text("Results Here!");
 });
+
 
 });
