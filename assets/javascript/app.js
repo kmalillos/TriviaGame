2d9d70ce14933977872d126ab065969adb5f6532
kmalillos <malillosk@gmail.com>
2018-10-21 14:49:04 -0600

    added radio buttons for answer choices and fixed bugs in JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,28 +40,24 @@ var incorrect = 0;
 
     // to start timer
 function startTimer () {
-        interval = setInterval(decrement, 1000); // decrease by 1 sec
+        interval = setInterval(decrementTimer, 1000); // decrease by 1 sec
 }
 
     // to countdown timer
-function decrement () {
+function decrementTimer () {
     timer--;
     $("#timer-text").text(timer);
 
         // when timer ends
     if (timer===0) {
-        stop();
-        $(".trivia-page").hide(); // trivia page hides
-        $(".results-page").show(); // results page shows
-
-        $("#correct-holder").text(correct);
-        $("#incorrect-holder").text(triviaGame.length - correct);
+        stopTimer();
     }
 }
 
     // to stop timer
-function stop () {
+function stopTimer () {
     clearInterval(interval);
+    submitGame();
 }
 
 function displayTrivia () {
@@ -76,66 +72,107 @@ function displayTrivia () {
         triviaText.append(questionHolder);
 
         // to DISPLAY triviaGame choices
-        for (var j=0; j < triviaGame.length; j++) {
-            var choicesHolder = $("<div>");
-                choicesHolder.addClass("choice");
-                choicesHolder.attr('value', triviaGame[i].choices[j]);
-                choicesHolder.append(triviaGame[i].choices[j]);
-            triviaText.append(choicesHolder);
+        for (var j=0; j < triviaGame[i].choices.length; j++) {
+            // var choicesHolder = $("<div>");
+            //     choicesHolder.addClass("choice");
+            //     choicesHolder.attr('value', triviaGame[i].choices[j]);
+            //     choicesHolder.append(triviaGame[i].choices[j]);
+            // triviaText.append(choicesHolder);
+
+            // var choicesHolder = "<input type='radio' value='" + triviaGame[i].choices[j] + "' name='question-" + i + "'>" + triviaGame[i].choices[j] + "<br>"
+            //     choicesHolder.attr("name", ("question-"+i))
+            //     choicesHolder.attr("value", triviaGame[i].choices[j]);
+            //     choicesHolder.text(triviaGame[i].choices[j])
+            // triviaText.append(choicesHolder);
+
+            triviaText.append("<input type='radio' value='" + triviaGame[i].choices[j] + 
+                                "' name='question-" + i + "'>" + triviaGame[i].choices[j] + "<br>");
 
         }   
     }
 };
 
-function selectChoices () {
+// function selectChoices () {
 
-    $(".choice").on("click", function() {
+//     $(".choice").on("click", function() {
 
-        //creates id to hook to css to change styles
-        console.log(this);
-        $(this).attr("id", "selectedChoice");
+//         //creates id to hook to css to change styles
+//         console.log(this);
+//         $(this).attr("id", "selectedChoice");
 
-        // determines if selected choice is correct 
-        for (var i=0; i < triviaGame.length; i ++) {
-            var selectedValue = $(this).attr("value");
-            if (selectedValue === triviaGame[i].answer) {
+//         // determines if selected choice is correct 
+//         for (var i=0; i < triviaGame.length; i ++) {
+//             var selectedValue = $(this).attr("value");
+//             if (selectedValue === triviaGame[i].answer) {
+//                 correct++;
+//             } 
+//         }
+//         console.log("Correct: " + correct);
+//     });
+// }
+
+    // to SUBMIT game
+
+function submitGame () {
+
+    for (var i=0; i<triviaGame.length; i++) {
+        $.each($("input[name='question-"+ i + "']:checked"), function() {
+            var userGuess = $(this).attr("value");
+            if (userGuess === triviaGame[i].answer) {
                 correct++;
-            } 
-        }
-        console.log("Correct: " + correct);
-    });
-}
+            } else {
+                incorrect++;
+            }
+        });
+    };
 
-// --- Main Process ---
+    if ( correct + incorrect !== triviaGame.length ) {
+        incorrect = triviaGame.length - correct;
+    }
 
-    // start button appears first
-    // trivia and result pages are hidden from beginning
-$(".trivia-page").hide();
-$(".results-page").hide();
-
-    // to START game after clicking start-button
-$("#start-button").on("click", function() {
-    $(".start-page").hide();   // start page hides
-    $(".trivia-page").show(); //trivia page shows
-});
-
-    // to CALL function that begins timer
-startTimer();
-
-    // to CALL function that displays trivia 
-displayTrivia();
-
-     // to CALL function that selects choices
-selectChoices();
-
-    // trivia ends when submitted (- or when time runs out, another function)
-$("#submit-button").on("click", function() {
-    $(this).hide();   // submit button hides
     $(".trivia-page").hide(); // trivia page hides
     $(".results-page").show(); // results page shows
 
     $("#correct-holder").text(correct);
-    $("#incorrect-holder").text(triviaGame.length - correct);
-});
+    $("#incorrect-holder").text(incorrect);
+};
+
+function restartGame () {
+
+}
+
+// --- Main Process ---
+
+        // start button appears first
+    // trivia and result pages are hidden from beginning
+    $(".trivia-page").hide();
+    $(".results-page").hide();
+    
+        // to START game after clicking start-button
+    $("#start-button").on("click", function() {
+        $(".start-page").hide();   // start page hides
+        $(".trivia-page").show(); //trivia page shows
+    });
+    
+        // to CALL function that begins timer
+    startTimer();
+    
+        // to CALL function that displays trivia 
+    displayTrivia();
+    
+    //      // to CALL function that selects choices
+    // selectChoices();
+    
+        // trivia ends when submitted (- or when time runs out, another function)
+    $("#submit-button").on("click", function() {
+        stopTimer();
+        $(this).hide();   
+    });
+
+    //     // restart game
+    // $("#restart-button").on("click", function() {
+    //     restartGame();
+    // });
+
 
 });
